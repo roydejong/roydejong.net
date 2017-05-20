704bd9e9d669f9b4c642ac4552f7f89ed7e17f48
Roy de Jong <roy@softwarepunt.nl>
2017-05-21 00:21:20 +0200

    Config: Update sample with the new integrations

--- config/config.sample.php
+++ config/config.sample.php
@@ -4,3 +4,17 @@ use roydejong\dotnet\Site\SiteConfig;
 
 $config = SiteConfig::instance();
 
+// Site basics
+$config->siteUrl = 'http://dev.roydejong.net';
+
+// Instagram integration
+$config->instagramEnabled = false;
+$config->instagramClientId = '';
+$config->instagramClientSecret = '';
+
+// Lastfm integration
+$config->lastfmEnabled = false
+$config->lastfmApiKey = '';
+$config->lastfmUsername = '';
+
+
